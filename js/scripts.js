4b45a3c81d3b533c45df31141a7530d5d605a777
Metro <jessicametro@gmail.com>
2012-12-12 10:33:55 -0600

    code dump (making this site responsive)

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#fancyTitle").fitText(0.5125);
+	$("#resourcesTitle").fitText(0.725);
+	$("#galleryHeader h2").fitText(0.45);
+	$("#galleryHeader h3").fitText(1.1);
+	
+});
