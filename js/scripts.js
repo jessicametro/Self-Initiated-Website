91eb8bf64f534480abab28c48327ec983ce50ea6
Metro <jessicametro@gmail.com>
2013-01-18 00:31:51 -0600

    FitText mostly fixed : stays in place
    new title
    max 180px, min 90px
    margins better (bottom margin isn't gigantic)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$("#fancyTitle").fitText(0.5125);
+	$("#fancyTitle").fitText(0.5125, { minFontSize: '90px', maxFontSize: '180px'});
 	$("#resourcesTitle").fitText(0.725);
 	$("#galleryHeader h2").fitText(0.45);
 	$("#galleryHeader h3").fitText(1.1);
