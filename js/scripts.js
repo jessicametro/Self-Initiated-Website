77580e6546cbf8dc13cfbe180743ea3d582d5574
Metro <jessicametro@gmail.com>
2013-02-05 04:29:58 -0600

    responsive headings : gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$("#fancyTitle").fitText(0.5125, { minFontSize: '90px', maxFontSize: '180px'});
 	$("#resourcesTitle").fitText(0.725);
-	$("#galleryHeader h2").fitText(0.45);
-	$("#galleryHeader h3").fitText(1.1);
+	$("#galleryHeader h2").fitText(0.44, { minFontSize: '42px', maxFontSize: '135px'});
+	$("#galleryHeader h3").fitText(0.83, { minFontSize: '13px', maxFontSize: '42px'});
 	
 });
 
