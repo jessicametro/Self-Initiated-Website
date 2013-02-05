b8f8af21d6797b5333493da404fc748956207c64
Metro <jessicametro@gmail.com>
2013-02-05 06:29:26 -0600

    fade in/out inner pages + animations + responsive headings : extended resources
    omg too much

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 	$("#fancyTitle").fitText(0.5125, { minFontSize: '90px', maxFontSize: '180px'});
-	$("#resourcesTitle").fitText(0.725);
+	$("#resourcesTitle").fitText(0.66, { minFontSize: '42px', maxFontSize: '135px'});
 	$("#galleryHeader h2").fitText(0.44, { minFontSize: '42px', maxFontSize: '135px'});
 	$("#galleryHeader h3").fitText(0.83, { minFontSize: '13px', maxFontSize: '42px'});
 	
+	
+	
+	$("#galleryHeader").click(function() {
+		goToGallery();
+	});
+	$("#flexslider").click(function() {
+		goToPage("#page2");
+	});
 });
 
 $(window).load(function() {
@@ -11,3 +19,22 @@ $(window).load(function() {
     	/* animation: "slide", */
     });
 });
+
+
+
+
+
+
+function goToPage(pagename) {
+	$(".tile").css({"margin-top":"-300px", "opacity":"0"});
+	$("#gallery").fadeOut(1000);
+	$(pagename).fadeIn(1000, function() {
+		
+	});
+}
+
+function goToGallery() {
+	$(".page").fadeOut(500);
+	$("#gallery").fadeIn(500);
+	$(".tile").css({"margin-top":"0px", "opacity":"1"});
+}
