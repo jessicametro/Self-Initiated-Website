3b91573d1dc3d54834a9414fef3d757292f55fe4
RJ Marsan <me@rj.io>
2013-02-08 05:57:35 -0600

    lots of new stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	fancyHistory();
+
 	$("#fancyTitle").fitText(0.5125, { minFontSize: '60px', maxFontSize: '180px'});
 	$("#resourcesTitle").fitText(0.66, { minFontSize: '42px', maxFontSize: '135px'});
 	$("#galleryHeader h2").fitText(0.44, { minFontSize: '42px', maxFontSize: '135px'});
@@ -41,19 +43,48 @@ $(window).load(function() {
 
 
 
+function fancyHistory() {
 
+	var statewatcher = function() {
+		var State = History.getState(); // Note: We are using History.getState() instead of event.state
+		History.log(State.data, State.title, State.url);
+		console.log("Hey state change wooo");
+		if (State.data.page) {
+			goToPage(state.data.page, true);
+		} else {
+			goToGallery(true);
+		}
+	};
 
-function goToPage(pagename) {
+	// Bind to StateChange Event
+	History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+		    statewatcher();
+	});
+
+	//History.pushState({home:true}, "", "");
+	//statewatcher();
+}
+
+function goToPage(pagename, donthistory) {
 	$(".tile").css({"margin-top":"-300px", "opacity":"0"});
 	$(".page").fadeOut(500);
 	$("#gallery").fadeOut(1000);
+	$(window).scrollTo("#galleryContainer");
 	$(pagename).fadeIn(1000, function() {
 		
 	});
+	if (!donthistory) {
+		console.log("Pushing history for page "+pagename);
+		History.pushState({page:pagename}, pagename, pagename.replace("#","/"));
+	}
 }
 
-function goToGallery() {
+function goToGallery(donthistory) {
 	$(".page").fadeOut(500);
 	$("#gallery").fadeIn(500);
 	$(".tile").css({"margin-top":"0px", "opacity":"1"});
+	if (!donthistory) {
+		History.pushState({home:true}, "", "");
+		console.log("Pushing history for home");
+	}
 }
