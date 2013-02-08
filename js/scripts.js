193605182defe95022620d4c1bdf0ca2d5b5f181
Metro <jessicametro@gmail.com>
2013-02-08 04:17:05 -0600

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$("#fancyTitle").fitText(0.5125, { minFontSize: '90px', maxFontSize: '180px'});
+	$("#fancyTitle").fitText(0.5125, { minFontSize: '60px', maxFontSize: '180px'});
 	$("#resourcesTitle").fitText(0.66, { minFontSize: '42px', maxFontSize: '135px'});
 	$("#galleryHeader h2").fitText(0.44, { minFontSize: '42px', maxFontSize: '135px'});
 	$("#galleryHeader h3").fitText(0.83, { minFontSize: '13px', maxFontSize: '42px'});
